6e42c34361454e9681de7cd8c4b0580d34d71667
SAURABH-0004 <skcoder004@gmail.com>
2025-02-09 11:30:04 +0530

    Add files via upload

---
+++ gallary/gallary.js
@@ -0,0 +1,8 @@
+
+        function openOverlay(src) {
+            document.getElementById("overlayImage").src = src;
+            document.getElementById("overlay").style.display = "flex";
+        }
+        function closeOverlay() {
+            document.getElementById("overlay").style.display = "none";
+        }
